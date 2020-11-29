600b3f33911f9af61d6579299d3d84338e6bec22
RealAlphaDEV <realalpha.ua@gmail.com>
2020-11-29 23:55:53 +0100

    change yaw & pitch :recycle:

--- src/main/java/eu/mshadeproduction/mwork/packet/player/PlayerEyeLocationPacket.java
+++ src/main/java/eu/mshadeproduction/mwork/packet/player/PlayerEyeLocationPacket.java
@@ -5,7 +5,19 @@ import eu.mshadeproduction.mwork.packet.entity.PlayerItem;
 
 public class PlayerEyeLocationPacket extends PlayerVectorPacket implements PacketResponse {
 
+    private final float yaw, pitch;
+
     public PlayerEyeLocationPacket(PlayerItem player, double x, double y, double z, float yaw, float pitch) {
-        super(player, x, y, z, yaw, pitch);
+        super(player, x, y, z);
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
+
+    public float getYaw() {
+        return yaw;
+    }
+
+    public float getPitch() {
+        return pitch;
     }
 }
