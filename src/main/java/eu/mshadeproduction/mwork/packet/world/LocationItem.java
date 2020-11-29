891dcef84b87f45b3899ad53a520a7d33f5b2a62
RealAlphaDEV <realalpha.ua@gmail.com>
2020-11-29 23:50:02 +0100

    remove Vector of Location :fire:

--- src/main/java/eu/mshadeproduction/mwork/packet/world/LocationItem.java
+++ src/main/java/eu/mshadeproduction/mwork/packet/world/LocationItem.java
@@ -7,23 +7,21 @@ public class LocationItem {
     private final WorldItem world;
     private final double x, y, z;
     private final float pitch, yaw;
-    private final Vector direction;
 
-    public LocationItem(WorldItem world, Vector direction, double x, double y, double z) {
-        this(world, direction, x, y, z, 0, 0);
-    }
     public LocationItem(WorldItem world, double x, double y, double z) {
-        this(world, new Vector(0,0,0), x, y, z, 0, 0);
+        this(world, x, y, z, 0, 0);
+    }
+    public LocationItem(WorldItem world, int x, int y, int z) {
+        this(world, x, y, z, 0, 0);
     }
 
-    public LocationItem(WorldItem world, Vector direction, double x, double y, double z, float pitch, float yaw) {
+    public LocationItem(WorldItem world, double x, double y, double z, float pitch, float yaw) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.yaw = yaw;
         this.pitch = pitch;
         this.world = world;
-        this.direction = direction;
     }
 
     public double getX() {
@@ -44,10 +42,6 @@ public class LocationItem {
 
     public float getPitch() {
         return pitch;
-    }
-
-    public Vector getDirection() {
-        return direction;
     }
 
     public WorldItem getWorldItem() {
