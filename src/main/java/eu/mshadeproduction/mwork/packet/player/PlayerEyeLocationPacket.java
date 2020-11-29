0df0e71ceaeb04caf764ed356503b1f83c3bc75d
RealAlphaDEV <realalpha.ua@gmail.com>
2020-11-29 22:37:23 +0100

    refractor PlayerEyeLocationPacket

--- src/main/java/eu/mshadeproduction/mwork/packet/player/PlayerEyeLocationPacket.java
+++ src/main/java/eu/mshadeproduction/mwork/packet/player/PlayerEyeLocationPacket.java
@@ -3,17 +3,11 @@ package eu.mshadeproduction.mwork.packet.player;
 import eu.mshadeproduction.mwork.packet.PacketResponse;
 import eu.mshadeproduction.mwork.packet.entity.PlayerItem;
 
-public class PlayerEyeVectorPacket extends PlayerVectorPacket implements PacketResponse {
+public class PlayerEyeLocationPacket extends PlayerVectorPacket implements PacketResponse {
 
 
-    private Vector direction;
-
-    public PlayerEyeVectorPacket(PlayerItem player, double x, double y, double z, Vector direction) {
+    public PlayerEyeLocationPacket(PlayerItem player, double x, double y, double z) {
         super(player, x, y, z);
-        this.direction = direction;
     }
 
-    public Vector getDirection() {
-        return direction;
-    }
 }
