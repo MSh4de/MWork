04a6e95a75cf5ef74fdbddd1440d4cde6ccc52b4
RealAlphaDEV <realalpha.ua@gmail.com>
2020-11-29 22:37:42 +0100

    add Vector direction

--- src/main/java/eu/mshadeproduction/mwork/packet/world/LocationItem.java
+++ src/main/java/eu/mshadeproduction/mwork/packet/world/LocationItem.java
@@ -1,22 +1,29 @@
 package eu.mshadeproduction.mwork.packet.world;
 
+import eu.mshadeproduction.mwork.packet.player.Vector;
+
 public class LocationItem {
 
     private final WorldItem world;
     private final double x, y, z;
     private final float pitch, yaw;
+    private final Vector direction;
 
+    public LocationItem(WorldItem world, Vector direction, double x, double y, double z) {
+        this(world, direction, x, y, z, 0, 0);
+    }
     public LocationItem(WorldItem world, double x, double y, double z) {
-        this(world, x, y, z, 0, 0);
+        this(world, new Vector(0,0,0), x, y, z, 0, 0);
     }
 
-    public LocationItem(WorldItem world, double x, double y, double z, float pitch, float yaw) {
+    public LocationItem(WorldItem world, Vector direction, double x, double y, double z, float pitch, float yaw) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.yaw = yaw;
         this.pitch = pitch;
         this.world = world;
+        this.direction = direction;
     }
 
     public double getX() {
@@ -37,6 +44,10 @@ public class LocationItem {
 
     public float getPitch() {
         return pitch;
+    }
+
+    public Vector getDirection() {
+        return direction;
     }
 
     public WorldItem getWorldItem() {
