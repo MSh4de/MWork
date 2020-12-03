c14bce3c1a12f608c33505fe8c7e2a54b4c773e5
RealAlphaDEV <realalpha.ua@gmail.com>
2020-12-03 23:54:05 +0100

    add PlayerpacketLocation :construction:

---
+++ src/main/java/eu/mshadeproduction/mwork/packet/player/PlayerLocationPacket.java
@@ -0,0 +1,26 @@
+package eu.mshadeproduction.mwork.packet.player;
+
+import eu.mshadeproduction.mwork.packet.Packet;
+import eu.mshadeproduction.mwork.packet.PacketResponse;
+import eu.mshadeproduction.mwork.packet.items.PlayerItem;
+import eu.mshadeproduction.mwork.packet.world.LocationItem;
+
+
+public class PlayerLocationPacket extends Packet implements PacketResponse {
+
+    private final PlayerItem player;
+    private final LocationItem location;
+
+    public PlayerLocationPacket(PlayerItem player, LocationItem location) {
+        this.player = player;
+        this.location = location;
+    }
+
+    public PlayerItem getPlayer() {
+        return player;
+    }
+
+    public LocationItem getLocation() {
+        return location;
+    }
+}
