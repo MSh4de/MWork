56df561b2aac18438bd1faebd9ec2b3e063cd55e
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-14 20:26:11 +0200

    update LongBinaryTag 🎢

--- src/main/java/eu/mshade/mwork/nametag/v2/entity/LongBinaryTag.java
+++ src/main/java/eu/mshade/mwork/nametag/v2/entity/LongBinaryTag.java
@@ -4,13 +4,20 @@ import eu.mshade.mwork.nametag.v2.BinaryTag;
 import eu.mshade.mwork.nametag.v2.BinaryTagType;
 
 public class LongBinaryTag implements BinaryTag<Long> {
+
+    private final long aLong;
+
+    public LongBinaryTag(long aLong) {
+        this.aLong = aLong;
+    }
+
     @Override
     public BinaryTagType getType() {
-        return null;
+        return BinaryTagType.LONG;
     }
 
     @Override
     public Long getValue() {
-        return null;
+        return aLong;
     }
 }
