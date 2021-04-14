1b12df045be7ee3f82c5b90548e68d233fa9fac8
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-14 20:32:16 +0200

    update BinaryTag 🧶

--- src/main/java/eu/mshade/mwork/nametag/v2/BinaryTagType.java
+++ src/main/java/eu/mshade/mwork/nametag/v2/BinaryTagType.java
@@ -20,20 +20,30 @@ public enum BinaryTagType {
     LIST(ListBinaryTag.class, 9),
     COMPOUND(CompoundBinaryTag.class, 10),
     INTEGER_ARRAY(IntegerArrayBinaryTag.class, 11),
-    LONG_ARRAY(LongArrayBinaryTag.class, 12);
+    LONG_ARRAY(LongArrayBinaryTag.class, 12),
+
+    ZSTD_BYTE_ARRAY(ZstdByteArrayBinaryTag.class, 13),
+    ZSTD_LIST(ZstdByteArrayBinaryTag.class, 14),
+    ZSTD_COMPOUND(ZstCompoundBinaryTag.class, 15),
+    ZSTD_INTEGER_ARRAY(ZstdIntegerArrayBinaryTag.class, 16),
+    ZSTD_LONG_ARRAY(ZstdLongArrayBinaryTag.class, 17);
+
+
 
     private static final Map<Integer, BinaryTagType> BY_ID = new HashMap<>();
+    private static final Map<Class<? extends BinaryTag>, BinaryTagType> BY_CLASS = new HashMap<>();
 
     static {
         for (BinaryTagType binaryTagType : BinaryTagType.values()) {
             BY_ID.put(binaryTagType.getId(), binaryTagType);
+            BY_CLASS.put(binaryTagType.getClazz(), binaryTagType);
         }
     }
 
-    private final Class<? extends BinaryTag<?>> aClass;
+    private final Class<? extends BinaryTag> aClass;
     private final int id;
 
-    BinaryTagType(Class<? extends BinaryTag<?>> aClass, int id) {
+    BinaryTagType(Class<? extends BinaryTag> aClass, int id) {
         this.aClass = aClass;
         this.id = id;
     }
@@ -42,8 +52,16 @@ public enum BinaryTagType {
         return id;
     }
 
+    public Class<? extends BinaryTag> getClazz() {
+        return aClass;
+    }
+
     public static BinaryTagType getTagTypeById(int id){
         return BY_ID.get(id);
+    }
+
+    public static BinaryTagType getTagTypeByClass(Class<? extends BinaryTag> aClass){
+        return BY_CLASS.get(aClass);
     }
 
 
