1f32647336c1ae01e1dc9840ad3253fe8928eb92
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-14 20:29:00 +0200

    update IntegerBinaryTag 🎲

--- src/main/java/eu/mshade/mwork/nametag/v2/entity/IntegerBinaryTag.java
+++ src/main/java/eu/mshade/mwork/nametag/v2/entity/IntegerBinaryTag.java
@@ -4,13 +4,20 @@ import eu.mshade.mwork.nametag.v2.BinaryTag;
 import eu.mshade.mwork.nametag.v2.BinaryTagType;
 
 public class IntegerBinaryTag implements BinaryTag<Integer> {
+
+    private final int anInt;
+
+    public IntegerBinaryTag(int anInt) {
+        this.anInt = anInt;
+    }
+
     @Override
     public BinaryTagType getType() {
-        return null;
+        return BinaryTagType.INTEGER;
     }
 
     @Override
     public Integer getValue() {
-        return null;
+        return anInt;
     }
 }
