44742c4fc9eec7ddef51387eaec5f299570221a6
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-14 20:30:14 +0200

    update CompoundBinaryTag 🧶

--- src/main/java/eu/mshade/mwork/nametag/v2/entity/CompoundBinaryTag.java
+++ src/main/java/eu/mshade/mwork/nametag/v2/entity/CompoundBinaryTag.java
@@ -3,17 +3,20 @@ package eu.mshade.mwork.nametag.v2.entity;
 import eu.mshade.mwork.nametag.v2.BinaryTag;
 import eu.mshade.mwork.nametag.v2.BinaryTagType;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CompoundBinaryTag implements BinaryTag<Map<String, BinaryTag<?>>> {
 
+    private final Map<String, BinaryTag<?>> binaryTagMap = new LinkedHashMap<>();
+
     @Override
     public BinaryTagType getType() {
-        return null;
+        return BinaryTagType.COMPOUND;
     }
 
     @Override
     public Map<String, BinaryTag<?>> getValue() {
-        return null;
+        return binaryTagMap;
     }
 }
