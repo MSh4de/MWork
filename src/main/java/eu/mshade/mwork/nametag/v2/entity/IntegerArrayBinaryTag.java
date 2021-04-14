c9f12c6123d7aaf327c9655f4c9c38a440cea4d8
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-14 20:29:22 +0200

    update IntegerArrayBinaryTag 🏆

--- src/main/java/eu/mshade/mwork/nametag/v2/entity/IntegerArrayBinaryTag.java
+++ src/main/java/eu/mshade/mwork/nametag/v2/entity/IntegerArrayBinaryTag.java
@@ -3,14 +3,21 @@ package eu.mshade.mwork.nametag.v2.entity;
 import eu.mshade.mwork.nametag.v2.BinaryTag;
 import eu.mshade.mwork.nametag.v2.BinaryTagType;
 
-public class IntegerArrayBinaryTag implements BinaryTag<Integer[]> {
-    @Override
-    public BinaryTagType getType() {
-        return null;
+public class IntegerArrayBinaryTag implements BinaryTag<int[]> {
+
+    private final int[] ints;
+
+    public IntegerArrayBinaryTag(int[] ints) {
+        this.ints = ints;
     }
 
     @Override
-    public Integer[] getValue() {
-        return new Integer[0];
+    public BinaryTagType getType() {
+        return BinaryTagType.INTEGER_ARRAY;
+    }
+
+    @Override
+    public int[] getValue() {
+        return ints;
     }
 }
