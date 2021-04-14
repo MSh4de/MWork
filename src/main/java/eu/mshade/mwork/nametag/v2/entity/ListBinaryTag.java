a1a69eab17cb70c315396b9282b228e80849eda8
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-14 20:28:45 +0200

    update ListBinaryTag ⛳

--- src/main/java/eu/mshade/mwork/nametag/v2/entity/ListBinaryTag.java
+++ src/main/java/eu/mshade/mwork/nametag/v2/entity/ListBinaryTag.java
@@ -3,17 +3,29 @@ package eu.mshade.mwork.nametag.v2.entity;
 import eu.mshade.mwork.nametag.v2.BinaryTag;
 import eu.mshade.mwork.nametag.v2.BinaryTagType;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ListBinaryTag implements BinaryTag<List<BinaryTag<?>>> {
+public class ListBinaryTag<T extends BinaryTag<?>> implements BinaryTag<List<T>> {
 
-    @Override
-    public BinaryTagType getType() {
-        return null;
+    private final List<T> list = new ArrayList<>();
+    private final Class<T> tClass;
+
+    public ListBinaryTag(Class<T> tClass) {
+        this.tClass = tClass;
     }
 
     @Override
-    public List<BinaryTag<?>> getValue() {
-        return null;
+    public BinaryTagType getType() {
+        return BinaryTagType.LIST;
+    }
+
+    @Override
+    public List<T> getValue() {
+        return list;
+    }
+
+    public Class<T> getElementType() {
+        return tClass;
     }
 }
