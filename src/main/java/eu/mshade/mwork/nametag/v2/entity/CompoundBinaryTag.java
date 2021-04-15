96ea7fad43fb3d54c4960080c4350b5b5e278485
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-15 23:31:53 +0200

    update CompoundBinary 🛒

--- src/main/java/eu/mshade/mwork/nametag/v2/entity/CompoundBinaryTag.java
+++ src/main/java/eu/mshade/mwork/nametag/v2/entity/CompoundBinaryTag.java
@@ -15,6 +15,56 @@ public class CompoundBinaryTag implements BinaryTag<Map<String, BinaryTag<?>>> {
         return BinaryTagType.COMPOUND;
     }
 
+    public void putBinaryTag(String key, BinaryTag<?> value){
+        binaryTagMap.put(key, value);
+    }
+
+    public BinaryTag<?> getBinaryTag(String key){
+        return binaryTagMap.get(key);
+    }
+
+    public void putByte(String key, byte value){
+        this.putBinaryTag(key, new ByteBinaryTag(value));
+    }
+
+    public byte getByte(String key){
+        return (byte) this.getBinaryTag(key).getValue();
+    }
+
+    public void putShort(String key, short value){
+        this.putBinaryTag(key, new ShortBinaryTag(value));
+    }
+
+    public short getShort(String key){
+        return (short) this.getBinaryTag(key).getValue();
+    }
+
+    public void putInt(String key, int value){ this.putBinaryTag(key, new IntegerBinaryTag(value)); }
+
+    public void putLong(String key, long value){
+        this.putBinaryTag(key, new LongBinaryTag(value));
+    }
+
+    public void putDouble(String key, double value){
+        this.putBinaryTag(key, new DoubleBinaryTag(value));
+    }
+
+    public void putByteArray(String key, byte[] value){
+        this.putBinaryTag(key, new ByteArrayBinaryTag(value));
+    }
+
+    public void putString(String key, String value){
+        this.putBinaryTag(key, new StringBinaryTag(value));
+    }
+
+    public void putIntArray(String key, int[] value){
+        this.putBinaryTag(key, new IntegerArrayBinaryTag(value));
+    }
+
+    public void putLongArray(String key, long[] value){
+        this.putBinaryTag(key, new LongArrayBinaryTag(value));
+    }
+
     @Override
     public Map<String, BinaryTag<?>> getValue() {
         return binaryTagMap;
