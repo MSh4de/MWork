6051bbbb8ed6f078954b359ab983750edaf79124
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-18 23:53:25 +0200

    add multiples properties 🥊

---
+++ src/main/java/eu/mshade/mwork/binarytag/marshal/BinaryTagName.java
@@ -0,0 +1,14 @@
+package eu.mshade.mwork.binarytag.marshal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface BinaryTagName {
+
+    String value();
+
+}
