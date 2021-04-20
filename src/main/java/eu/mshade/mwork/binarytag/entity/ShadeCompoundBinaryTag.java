212c46d6516f5c4e0a79585c965cf9848dd6e667
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-20 21:02:32 +0200

    add ShadeCompoundBinaryTag 🥏

---
+++ src/main/java/eu/mshade/mwork/binarytag/entity/ShadeCompoundBinaryTag.java
@@ -0,0 +1,21 @@
+package eu.mshade.mwork.binarytag.entity;
+
+import eu.mshade.mwork.binarytag.BinaryTag;
+import eu.mshade.mwork.binarytag.BinaryTagType;
+
+import java.util.Map;
+
+public class ShadeCompoundBinaryTag extends CompoundBinaryTag {
+
+    public ShadeCompoundBinaryTag(Map<String, BinaryTag<?>> binaryTagMap) {
+        super(binaryTagMap);
+    }
+
+    public ShadeCompoundBinaryTag() {
+    }
+
+    @Override
+    public BinaryTagType getType() {
+        return BinaryTagType.SHADE_COMPOUND;
+    }
+}
