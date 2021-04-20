e097bc2ca36d4a7899fca69fb6807d6dafd805c7
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-20 21:03:54 +0200

    add ShadeByteArrayBinaryTagAdaptor 🥊

---
+++ src/main/java/eu/mshade/mwork/binarytag/marshal/array/ShadeByteArrayBinaryTagAdaptor.java
@@ -0,0 +1,20 @@
+package eu.mshade.mwork.binarytag.marshal.array;
+
+import eu.mshade.mwork.binarytag.BinaryTag;
+import eu.mshade.mwork.binarytag.entity.ByteArrayBinaryTag;
+import eu.mshade.mwork.binarytag.marshal.BinaryTagMarshal;
+
+import java.lang.reflect.Type;
+
+public class ShadeByteArrayBinaryTagAdaptor extends ByteArrayBinaryTagAdaptor {
+
+    @Override
+    public BinaryTag<?> serialize(BinaryTagMarshal binaryTagMarshal, Type type, Object o) throws Exception {
+        return ((ByteArrayBinaryTag) super.serialize(binaryTagMarshal, type, o)).toShade();
+    }
+
+    @Override
+    public Object deserialize(BinaryTagMarshal binaryTagMarshal, Type type, BinaryTag<?> binaryTag) throws Exception {
+        return super.deserialize(binaryTagMarshal, type, binaryTag);
+    }
+}
