856ee3812415536c4de407a8b25ad09104c21406
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-20 20:59:19 +0200

    refractor ZstdLongArrayBinaryTagBuffer 🧶

--- src/main/java/eu/mshade/mwork/binarytag/buffer/ZstdLongArrayBinaryTagBuffer.java
+++ src/main/java/eu/mshade/mwork/binarytag/buffer/ZstdLongArrayBinaryTagBuffer.java
@@ -4,6 +4,7 @@ import com.github.luben.zstd.Zstd;
 import eu.mshade.mwork.binarytag.BinaryTag;
 import eu.mshade.mwork.binarytag.BinaryTagBuffer;
 import eu.mshade.mwork.binarytag.BinaryTagBufferDriver;
+import eu.mshade.mwork.binarytag.BinaryTagType;
 import eu.mshade.mwork.binarytag.entity.LongArrayBinaryTag;
 
 import java.io.ByteArrayInputStream;
@@ -11,15 +12,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
-public class ZstdLongArrayBinaryBuffer implements BinaryTagBuffer {
+public class ZstdLongArrayBinaryTagBuffer implements BinaryTagBuffer {
     @Override
     public void write(BinaryTagBufferDriver binaryTagBufferDriver, DataOutputStream outputStream, BinaryTag<?> binaryTag) throws Exception {
-        LongArrayBinaryTag longArrayBinaryTag = (LongArrayBinaryTag) binaryTag;
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
-        for (long i : longArrayBinaryTag.getValue()) {
-            dataOutputStream.writeLong(i);
-        }
+        binaryTagBufferDriver.getBufferByType(BinaryTagType.LONG_ARRAY).write(binaryTagBufferDriver, dataOutputStream, binaryTag);
         byte[] compress = Zstd.compress(byteArrayOutputStream.toByteArray());
         outputStream.writeInt(byteArrayOutputStream.size());
         outputStream.writeInt(compress.length);
@@ -35,11 +33,6 @@ public class ZstdLongArrayBinaryBuffer implements BinaryTagBuffer {
         inputStream.readFully(payload);
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(Zstd.decompress(payload, realSize));
         DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
-        long[] longs = new long[realSize];
-        for (int i = 0; i < realSize; i++) {
-            longs[i] = dataInputStream.readLong();
-        }
-        dataInputStream.close();
-        return new LongArrayBinaryTag(longs);
+        return ((LongArrayBinaryTag) binaryTagBufferDriver.getBufferByType(BinaryTagType.LONG_ARRAY).read(binaryTagBufferDriver, dataInputStream)).toZstd();
     }
 }
