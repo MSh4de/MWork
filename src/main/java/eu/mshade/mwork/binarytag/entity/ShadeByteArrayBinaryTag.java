ed10d9a447e934a3ca13bc73b81f1b1bb098f931
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-20 21:01:26 +0200

    add ShadeByteArrayBinaryTag 🎳

---
+++ src/main/java/eu/mshade/mwork/binarytag/entity/ShadeByteArrayBinaryTag.java
@@ -0,0 +1,15 @@
+package eu.mshade.mwork.binarytag.entity;
+
+import eu.mshade.mwork.binarytag.BinaryTagType;
+
+public class ShadeByteArrayBinaryTag extends ByteArrayBinaryTag {
+
+    public ShadeByteArrayBinaryTag(byte[] bytes) {
+        super(bytes);
+    }
+
+    @Override
+    public BinaryTagType getType() {
+        return BinaryTagType.SHADE_BYTE_ARRAY;
+    }
+}
