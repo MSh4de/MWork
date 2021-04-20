b70b2f2152e0082310c1e94968715180cdfb2f04
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-20 21:00:23 +0200

    add ShadeCompoundBinaryTagBuffer 🏓

---
+++ src/main/java/eu/mshade/mwork/binarytag/buffer/ShadeCompoundBinaryTagBuffer.java
@@ -0,0 +1,33 @@
+package eu.mshade.mwork.binarytag.buffer;
+
+import eu.mshade.mwork.binarytag.BinaryTag;
+import eu.mshade.mwork.binarytag.BinaryTagBufferDriver;
+import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class ShadeCompoundBinaryTagBuffer extends CompoundBinaryTagBuffer{
+
+    @Override
+    public void write(BinaryTagBufferDriver binaryTagBufferDriver, DataOutputStream outputStream, BinaryTag<?> binaryTag) throws Exception {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+        super.write(binaryTagBufferDriver, dataOutputStream, binaryTag);
+        outputStream.writeInt(byteArrayOutputStream.size());
+        outputStream.write(byteArrayOutputStream.toByteArray());
+        dataOutputStream.close();
+    }
+
+    @Override
+    public CompoundBinaryTag read(BinaryTagBufferDriver binaryTagBufferDriver, DataInputStream inputStream) throws Exception {
+        int size = inputStream.readInt();
+        byte[] payload = new byte[size];
+        inputStream.readFully(payload);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(payload);
+        DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
+        return super.read(binaryTagBufferDriver, dataInputStream).toShade();
+    }
+}
