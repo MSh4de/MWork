49939a01c1fe018af146fb0a01675410304dcd51
RealAlphaUA <realalpha.ua@gmail.com>
2021-04-21 13:16:29 +0200

    update of extends of CompoundBinaryTagAdaptor 🎢

--- src/main/java/eu/mshade/mwork/binarytag/marshal/ZstdCompoundBinaryTagAdaptor.java
+++ src/main/java/eu/mshade/mwork/binarytag/marshal/ZstdCompoundBinaryTagAdaptor.java
@@ -1,20 +1,14 @@
 package eu.mshade.mwork.binarytag.marshal;
 
 import eu.mshade.mwork.binarytag.BinaryTag;
-import eu.mshade.mwork.binarytag.BinaryTagType;
 import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag;
 
 import java.lang.reflect.Type;
 
-public class ZstdCompoundBinaryTagAdaptor implements BinaryTagAdaptor<Object>{
+public class ZstdCompoundBinaryTagAdaptor extends CompoundBinaryTagAdaptor{
 
     @Override
     public BinaryTag<?> serialize(BinaryTagMarshal binaryTagMarshal, Type type, Object o) throws Exception {
-        return ((CompoundBinaryTag) binaryTagMarshal.getBinaryTagAdaptor(BinaryTagType.COMPOUND.getClazz()).serialize(binaryTagMarshal, type, o)).toZstd();
-    }
-
-    @Override
-    public Object deserialize(BinaryTagMarshal binaryTagMarshal, Type type, BinaryTag<?> binaryTag) throws Exception {
-        return binaryTagMarshal.getBinaryTagAdaptor(BinaryTagType.COMPOUND.getClazz()).deserialize(binaryTagMarshal, type, binaryTag);
+        return ((CompoundBinaryTag) super.serialize(binaryTagMarshal, type, o)).toZstd();
     }
 }
