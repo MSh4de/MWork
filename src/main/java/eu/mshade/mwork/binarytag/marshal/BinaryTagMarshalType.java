045c69ba9ccd4702314d08bd33a346161e55e53f
RealAlphaUA <realalpha.ua@gmail.com>
2021-05-11 23:31:53 +0200

    refrator 🥅

--- src/main/java/eu/mshade/mwork/binarytag/marshal/BinaryTagMarshalType.java
+++ src/main/java/eu/mshade/mwork/binarytag/marshal/BinaryTagMarshalType.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface BinaryTagAdapt {
+public @interface BinaryTagTypeAdaptor {
 
     Class<? extends BinaryTagAdaptor<?>> value();
 
