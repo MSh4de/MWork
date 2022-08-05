4d5875df1ba7855cd52d0761dbdb859cb931f85b
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-05 20:30:04 +0200

    complet Mediator 🎨

--- src/main/java/eu/mshade/mwork/mediator/Mediator.java
+++ src/main/java/eu/mshade/mwork/mediator/Mediator.java
@@ -1,9 +1,24 @@
 package eu.mshade.mwork.mediator;
 
-public interface Mediator {
+import java.util.function.Supplier;
 
-    Mediator MEDIATOR = (source, key) -> {};
+public interface Mediator<S> {
 
-    void notify(Object source, String key);
+    Mediator NONE = new Mediator() {
+        @Override
+        public Object notify(Object source, Supplier callback, String key, Object[] args) {
+            return callback.get();
+        }
+    };
+
+    <T> T notify(S source, Supplier<T> callback, String key, Object[] args);
+
+    default void notify(S source, Runnable runnable, String key, Object[] args) {
+        notify(source, () -> {
+            runnable.run();
+            return null;
+        }, key, args);
+    }
+
 
 }
