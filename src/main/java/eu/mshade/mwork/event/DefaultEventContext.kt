19fa056b2a3f47b6c55f9a051f3024b625bcb68c
Azodox <likemoi99@gmail.com>
2022-08-21 00:58:58 +0200

    Convert DefaultEventContext to Kotlin

---
+++ src/main/java/eu/mshade/mwork/event/DefaultEventContext.kt
@@ -0,0 +1,22 @@
+package eu.mshade.mwork.event
+
+
+class DefaultEventContext<E>(override var eventType: Class<E>, override var eventListener: EventListener<E>) : EventContext<E> {
+    override var eventPriority: EventPriority = EventPriority.NORMAL
+    override var eventFilter: EventFilter = EventFilter.ONLY
+
+    override fun withEventFilter(eventFilter: EventFilter): EventContext<E> {
+        this.eventFilter = eventFilter
+        return this;
+    }
+
+    override fun withEventPriority(eventPriority: EventPriority): EventContext<E> {
+        this.eventPriority = eventPriority
+        return this;
+    }
+
+    override fun toString(): String {
+        return "DefaultEventContext(eventPriority=$eventPriority, eventFilter=$eventFilter, eventType=$eventType, eventListener=$eventListener)"
+    }
+
+}
