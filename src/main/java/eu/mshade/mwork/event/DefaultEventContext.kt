02449e5d354a837f84c8b58cb7d599e7849ba67d
Azodox <likemoi99@gmail.com>
2022-08-21 17:00:01 +0200

    Replace EventPriority by EventPriorities

--- src/main/java/eu/mshade/mwork/event/DefaultEventContext.kt
+++ src/main/java/eu/mshade/mwork/event/DefaultEventContext.kt
@@ -2,7 +2,7 @@ package eu.mshade.mwork.event
 
 
 class DefaultEventContext<E>(override var eventType: Class<E>, override var eventListener: EventListener<E>) : EventContext<E> {
-    override var eventPriority: EventPriority = EventPriority.NORMAL
+    override var eventPriority: EventPriority = EventPriorities.NORMAL
     override var eventFilter: EventFilter = EventFilter.ONLY
 
     override fun withEventFilter(eventFilter: EventFilter): EventContext<E> {
