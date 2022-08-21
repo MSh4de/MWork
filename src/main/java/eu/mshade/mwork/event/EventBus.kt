acc549ec01615540fd228e27f9ebc64e713a1605
Azodox <likemoi99@gmail.com>
2022-08-21 17:01:19 +0200

    Created a comparator for listeners, removed every parameter containers, updated publish method and wrote javadoc

--- src/main/java/eu/mshade/mwork/event/EventBus.kt
+++ src/main/java/eu/mshade/mwork/event/EventBus.kt
@@ -1,69 +1,83 @@
 package eu.mshade.mwork.event
 
-import eu.mshade.mwork.ParameterContainer
 import java.util.*
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.stream.Collectors
 
+/**
+ * The event bus manages listeners and allows you to trigger your own events.
+ * @see [subscribe] to register a listener.
+ * @see [publish] to trigger an event.
+ * @author Luke and Oleksandr
+ * @since 0.0.1
+ */
 class EventBus<T> {
-    private val eventContexts: Queue<EventContext<T>> = ConcurrentLinkedQueue()
+    private val eventContexts: Queue<EventContext<*>> = ConcurrentLinkedQueue()
+
+    /**
+     * Register a listener of an event.
+     * @param eventClass the event class to listen to
+     * @param eventListener your listener
+     * @return An [EventContext] to manage listening options
+     */
     fun <E : T> subscribe(eventClass: Class<E>, eventListener: EventListener<E>): EventContext<E> {
         val eventContext = DefaultEventContext(eventClass, eventListener)
-        eventContexts.add(eventContext as DefaultEventContext<T>)
+        eventContexts.add(eventContext)
         return eventContext
     }
 
-    fun <E : T?> publish(event: E) {
-        this.publish(event, ParameterContainer.EMPTY)
+    /**
+     * Allows you to unsubscribe any registered listener.
+     * @param eventClass The event type of the listener
+     * @param eventListener The listener to unsubscribe
+     */
+    fun <E : T> unsubscribe(eventClass: Class<E>, eventListener: EventListener<E>) {
+        eventContexts.removeIf { it.eventType == eventClass && it.eventListener == eventListener }
     }
 
-    fun <E : T?> publish(event: E, parameterContainer: ParameterContainer) {
-        val eventContexts = eventContexts.stream()
-            .filter { eventContext: EventContext<T> ->
-                hasMatch(
-                    eventContext.eventType,
-                    if (eventContext.eventType.isInterface && event!!::class.java.interfaces.isNotEmpty()) event.javaClass.interfaces[0] else event!!::class.java,
-                    eventContext.eventFilter
-                )
-            }
-            .collect(Collectors.toList())
-        if (eventContexts.isNotEmpty()) {
-            /*
-            TODO : Update this (currently contains errors)
-            for (eventPriority in EventPriority.values()) {
-                onEvent(event, parameterContainer, eventPriority, eventContexts)
-            }*/
-        }
+    /**
+     * Pass an instance of an event to trigger all subscribed listeners.
+     * @param event The event to trigger.
+     */
+    fun <E : T> publish(event: E) {
+        eventContexts.stream()
+            // Filter out contexts that don't match the event type or any subtype (EventFilter logic).
+            .filter { eventContext -> hasMatch(
+                eventContext.eventType,
+                if(eventContext.eventType.isInterface) eventContext.eventType.javaClass else
+                    eventContext.eventType.interfaces[0].javaClass, eventContext.eventFilter
+            )}
+                // Sort the contexts by priority.
+            .sorted { o1, o2 -> EventComparator().compare(o1, o2) }
+                // Trigger the listener on each context.
+            .forEach { eventContext -> eventContext.eventListener.onEvent(event) }
     }
 
-    fun <E : T?> publishAsync(event: E) {
-        this.publishAsync(event, ParameterContainer.EMPTY)
+    /**
+     * Do the same as [publish] but asynchronously.
+     */
+    fun <E : T> publishAsync(event: E) {
+        CompletableFuture.runAsync { publish(event) }
     }
 
-    fun <E : T?> publishAsync(event: E, parameterContainer: ParameterContainer) {
-        CompletableFuture.runAsync { publish(event, parameterContainer) }
-        //publish(event, parameterContainer);
-    }
-
-    private fun <E : T?> onEvent(
-        event: E,
-        eventContainer: ParameterContainer,
-        eventPriority: EventPriority,
-        eventContexts: List<EventContext<T>>
-    ) {
-        //eventContexts.stream().filter(eventContext -> eventContext.getEventPriority() == eventPriority).forEach(eventContext -> scheduledExecutorService.execute(() -> eventContext.getEventListener().onEvent(event, eventContainer)));
-        eventContexts.stream().filter { eventContext: EventContext<T> -> eventContext.eventPriority === eventPriority }
-            .forEach { eventContext: EventContext<T> -> eventContext.eventListener.onEvent(event!!, eventContainer) }
-    }
-
-    fun <E : T?> getEventContexts(eventType: Class<E>, eventFilter: EventFilter): List<EventContext<T>> {
-        return eventContexts.stream()
-            .filter { eventContext: EventContext<T> -> hasMatch(eventContext.eventType, eventType, eventFilter) }
-            .collect(Collectors.toList())
-    }
-
+    /**
+     * Check if whether from and to are equals or if from is a subtype of to<br>
+     * depending on the eventFilter.
+     * @param from The event's class
+     * @param to The event's class or any sub interface of it
+     * @param eventFilter The event's filter
+     */
     private fun hasMatch(from: Class<*>, to: Class<*>, eventFilter: EventFilter): Boolean {
         return if (eventFilter === EventFilter.ONLY) from == to else from.isAssignableFrom(to)
+    }
+
+    /**
+     * A comparator to sort listeners by priority.
+     */
+    class EventComparator : Comparator<EventContext<*>> {
+        override fun compare(o1: EventContext<*>, o2: EventContext<*>): Int {
+            return o1.eventPriority.weight - o2.eventPriority.weight
+        }
+
     }
 }
