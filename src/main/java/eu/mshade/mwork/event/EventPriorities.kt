1a91dbe2feaf8033697d7822938cb2058058ddd1
Azodox <likemoi99@gmail.com>
2022-08-21 14:12:57 +0200

    Add values method and a method to get a priority by its weight

--- src/main/java/eu/mshade/mwork/event/EventPriorities.kt
+++ src/main/java/eu/mshade/mwork/event/EventPriorities.kt
@@ -3,8 +3,19 @@ package eu.mshade.mwork.event
 interface EventPriorities {
 
     companion object {
+        private val BY_WEIGHT: MutableMap<Int, EventPriority>
+            get() = mutableMapOf()
+
         val HIGH: EventPriority = EventPriority.from(3)
         val NORMAL: EventPriority = EventPriority.from(2)
         val LOW: EventPriority = EventPriority.from(1)
+
+        fun values(): List<EventPriority> = listOf(HIGH, NORMAL, LOW)
+
+        init {
+            values().forEach { priority -> BY_WEIGHT[priority.weight] = priority }
+        }
+
+        fun byWeight(weight: Int): EventPriority = BY_WEIGHT[weight] ?: throw IllegalArgumentException("No priority with weight $weight")
     }
 }
