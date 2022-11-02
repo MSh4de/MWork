7d19f4bbda370d202a972afe0bdb53ae94136dcd
RealAlphaUA <realalpha.ua@gmail.com>
2022-11-02 01:03:00 +0100

    update of new convention 🛒

--- src/main/java/eu/mshade/mwork/UUIDBinaryTagMarshal.java
+++ src/main/java/eu/mshade/mwork/UUIDBinaryTagMarshal.java
@@ -2,21 +2,20 @@ package eu.mshade.mwork;
 
 import eu.mshade.mwork.binarytag.BinaryTag;
 import eu.mshade.mwork.binarytag.BinaryTagDriver;
-import eu.mshade.mwork.binarytag.entity.StringBinaryTag;
 import eu.mshade.mwork.binarytag.BinaryTagMarshal;
+import eu.mshade.mwork.binarytag.StringBinaryTag;
 
-import java.lang.reflect.Type;
 import java.util.UUID;
 
 public class UUIDBinaryTagMarshal implements BinaryTagMarshal<UUID> {
 
     @Override
-    public BinaryTag<?> serialize(BinaryTagDriver binaryTagDriver, UUID uuid) throws Exception {
+    public BinaryTag<?> serialize(BinaryTagDriver binaryTagDriver, UUID uuid) {
         return new StringBinaryTag(uuid.toString());
     }
 
     @Override
-    public UUID deserialize(BinaryTagDriver binaryTagDriver, BinaryTag<?> binaryTag) throws Exception {
+    public UUID deserialize(BinaryTagDriver binaryTagDriver, BinaryTag<?> binaryTag) {
         return UUID.fromString((String) binaryTag.getValue());
     }
 }
